4721625704458fbe454a2abb9e02e01639f499ab
sontosh36 <sontosh159700@gmail.com>
2026-04-14 22:19:51 +0600

    Project initial setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1 className="font-bold text-5xl">Get started</h1>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
